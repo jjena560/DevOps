e1c9c5601981f2277ae6c382dcf55e8bdfa3a454
Jyotiprakash Jena <40398542+jjena560@users.noreply.github.com>
2021-09-29 00:33:51 +0530

    initil commit

---
+++ src/main/java/com/demo/devops/App.java
@@ -0,0 +1,17 @@
+package com.demo.devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+	public int add(int a, int b) {
+		int c = a + b;
+		return a + b;
+	}
+
+}
